0c46f2e945e17cac404d6219258bf9e8c53b58be
Harsh Vardhan Singhal <51576476+harsh778@users.noreply.github.com>
2021-01-05 20:03:46 +0530

    develop env and add netcat

--- socket.js
+++ socket.js
@@ -3,3 +3,15 @@ socket.bind("127.0.0.1", 6789);
 socket.listen(1); //listen at 1
 var conn = socket.accept(); //acCepT connection lol
 stdin.read(); //exit
+
+conn.send("Setup Active! gg mate \n");
+var reply = client.recv(6789);
+client.send(reply); //send before closing
+conn.close();
+
+get_stuff() {
+    var client = connect("127.0.0.1", 6789);
+    client.send("Fetch IP\n");
+    var line = client.recv(6789);
+    assert(line == "Received\n");
+}
